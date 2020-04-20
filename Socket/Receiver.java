c86a788813cebc14f319e496a6259376a7d84563
himancifer <himangshushekharjha@gmail.com>
2020-04-20 23:10:14 +0530

    Added the socket functionality

---
+++ Socket/Receiver.java
@@ -0,0 +1,88 @@
+// Java Socket code for Receiver
+
+import java.net.*; 
+import java.io.*; 
+import java.util.*; 
+   
+public class Receiver { 
+      
+    // Initialize socket and I/O streams 
+    private Socket s = null; 
+    private DataInputStream dis = null; 
+    private DataOutputStream dos = null; 
+      
+    // Constructor to put ip address and port 
+    public Receiver(InetAddress ip,int port)throws IOException 
+    { 
+          
+        // Opens a socket for connection 
+        s = new Socket(ip,port); 
+        System.out.println("Receiver listening to port "+ port);
+          
+        dis = new DataInputStream(s.getInputStream()); 
+        dos = new DataOutputStream(s.getOutputStream()); 
+          
+        while (true) 
+        {   Scanner scan = new Scanner(System.in); 
+            int sum=0; 
+              
+            // Reads the data length sent by sender 
+            int length = dis.readInt(); 
+              
+            // Initializes the arrays based on data length received 
+            long data[] = new long[length]; 
+              
+            System.out.println("Data received (Last data represents the checksum) is"); 
+              
+            for(int i = 0; i< data.length; i++) 
+            {    
+                // Reading the data being sent one by one 
+                data[i] = dis.readInt(); 
+                System.out.println(Long.toBinaryString(data[i]));                 
+                sum+=data[i];
+            } 
+            sum%=65536; //To remove carry and overflow
+            System.out.println("Sum = " + "(" + sum + ")10" + " = " + "(" + Long.toBinaryString(sum) + ")2");
+
+            int output=0;
+            
+            for(int i=data.length-2;i>=0;i--){
+                output*=65536;
+                output+=data[i];
+            }
+            System.out.println("=======================================================");
+
+            System.out.println("Final Decoded Data :: "+ output);
+
+
+            // Checking whether final result is 65535 or something else and sending feedback accordingly
+
+            if(sum == 65535) // Since (65535)10 = (1111111111111111)2
+            {    
+                dos.writeUTF("success");    // Since no error was found and the data was received successfully 
+                break; 
+            }      
+            else
+            {    
+                dos.writeUTF("failure");  // There was some error in transmitting or sending the data 
+                break; 
+            }
+
+             
+        } 
+          
+        // Closing all connections 
+        dis.close(); 
+        dos.close(); 
+        s.close(); 
+    } 
+      
+    // Main method to set up port and calling the constructor of receiver.
+    public static void main(String args[])throws IOException 
+    {    
+        // Getting ip address on which the receiver is running 
+
+        InetAddress ip = InetAddress.getLocalHost(); // This give "localhost" as the ip address  
+        Receiver receive = new Receiver(ip,8080); 
+    }     
+} 
---
+++ Socket/Sender.java
@@ -0,0 +1,100 @@
+// Java Socket code for Sender 
+
+import java.io.*; 
+import java.net.*; 
+import java.util.*; 
+
+public class Sender 
+{ 
+	// initialize socket and I/O streams 
+	private Socket socket = null; 
+	private ServerSocket servsock = null; 
+	private DataInputStream dis = null; 
+	private DataOutputStream dos = null; 
+	
+	public Sender(int port) throws IOException 
+	{ 
+		servsock = new ServerSocket(port); 
+        System.out.println("Server start at port "+port+".");
+		
+		//Block until a client connects to the server 
+		socket = servsock.accept(); 
+		
+		dis = new DataInputStream(socket.getInputStream()); 
+		dos = new DataOutputStream(socket.getOutputStream()); 
+		
+		while (true) 
+		{  
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter data to be sent"); 
+            long data = scan.nextInt();
+		
+			
+			int size=(int)Math.ceil(Math.log(data)/Math.log(65536)); //To find the number of Datawords to to be sent 
+            System.out.println("Number of datawords = "+size);
+            long output[]= new long[size+1];   // In order to store the checksum as well and subsequently send it to reciever for error detection
+            long sum=0;
+            int i=0;
+            System.out.println("====================================================================");
+            System.out.println("Breaking the data to be sent in units of 16 bits ........");
+            
+            while(data!=0){   //initialized the counter for output array and added cond. for data to be divided into 16 bits
+                
+                output[i]=data%65536;   //taken rightmost 16 bits 
+                
+                System.out.println((i+1)+ " Unit = " + output[i]);
+                
+                sum+=output[i++];
+                
+                sum%=65536;         //to remove overflow and carry
+                
+                data/=65536;
+        }
+
+        output[i]=sum^65535;
+        System.out.println("Checksum = "+ output[i]);
+
+        // Sends the data length to Receiver 
+        dos.writeInt(output.length); 
+
+        System.out.println("Datawords to be sent in binary :: ");
+              
+			
+        // Sends the data one by one to receiver
+        System.out.print("| "); 
+        for (int j = 0; j < output.length; j++){
+            System.out.print(Long.toBinaryString(output[j])+" | ");
+            dos.writeLong(output[j]); 
+        } 
+
+        System.out.println();
+		System.out.println("======================================================================");
+		System.out.println("Data being sent along with Checkum.....");
+        System.out.println("======================================================================");
+
+		// Displaying appropriate message depending on feedback received from the receiver end. 
+			if (dis.readUTF().equals("success")) 
+			{ 
+				System.out.println("Feedback :: Message received Successfully!"); 
+				break; 
+			} 
+			
+			else if (dis.readUTF().equals("failure")) 
+			{ 
+				System.out.println("Message was not received successfully!"); 
+				break; 
+			} 
+		} 
+		
+		// Closing all connections 
+		dis.close(); 
+		dos.close(); 
+		socket.close(); 
+	} 
+
+	//Main method for invoking the Sender constructor and initializing all the values
+	public static void main(String args[]) throws IOException 
+	{ 
+		Sender send = new Sender(8080); 
+	} 
+} 
